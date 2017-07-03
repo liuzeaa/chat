eed3d2f6c80355953619dab99c89ce7fa00d66a3
liuzeaa <1098367891@qq.com>
2017-07-03 11:12:54 +0800

    0703ss

---
+++ chat/src/store/index.js
@@ -0,0 +1,17 @@
+import Vuex from 'vuex'
+import vux from 'vue'
+vue.use(Vuex)
+export default store = new Vuex.store({
+  state:{
+
+  },
+  getters:{
+
+  },
+  mutations:{
+
+  },
+  actions:{
+
+  }
+})
